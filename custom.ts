ac51bb576a741a6a02ae6db012672204f42084ae
akahoshi1421 <akaho1421@gmail.com>
2024-02-16 17:23:37 +0900

    add: 色を統一 + 名前の統一

--- custom.ts
+++ custom.ts
@@ -1,4 +1,4 @@
-//% color=#A22C01 weight=400 icon="\uf04b" block=エージェントチェック
+//% color=#D83B01 weight=400 icon="\uf04b" block="エージェントチェック"
 namespace agentInspectDetect{
     //% blockId=agentInspect
     //% block="エージェントにどんな $kind か、 $direction を確認させる"
@@ -6,7 +6,7 @@ namespace agentInspectDetect{
         return agent.inspect(kind, direction);
     }
 
-    //% blockId=agentDetectHiragana
+    //% blockId=agentDetect
     //% block="エージェントに $kind があるか、 $direction を確認させる"
     export function agentDetectHiragana(kind: AgentDetection, direction: SixDirection): boolean {
         return agent.detect(kind, direction);
