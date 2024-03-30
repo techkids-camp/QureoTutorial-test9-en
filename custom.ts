84fe7b69baec4fdcd155e0e7722ede00ff1ae7d2
Drkoukichi <78055680+Drkoukichi@users.noreply.github.com>
2024-03-30 21:22:17 +0900

    init

--- custom.ts
+++ custom.ts
@@ -21,15 +21,10 @@ namespace agentInspectDetect{
  */
 //% color=#D83B01 weight=400 icon="\uf04b" block="ひらがな"
 namespace hiragana_blocks{
-    //% blockId=hiragana_blocks
-    //% block="エージェントにどんな $kind か、 $direction を確認させる"
-    export function agentInspect(kind: AgentInspection, direction: SixDirection): number {
-        return agent.inspect(kind, direction);
-    }
 
     //% blockId=hiragana_blocks
-    //% block="エージェントに $kind があるか、 $direction を確認させる"
-    export function agentDetect(kind: AgentDetection, direction: SixDirection): boolean {
-        return agent.detect(kind, direction);
+    //% block="じぶんを $to にテレポートさせる"
+    export function agentDetect(pos: Position): void {
+        return player.teleport(pos);
     }
 }
