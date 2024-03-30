c57ed84be08b6cfec3fc636263aba29d025de35e
Drkoukichi <78055680+Drkoukichi@users.noreply.github.com>
2024-03-30 21:32:58 +0900

    ひらがな_プレイヤーとひらがな＿エージェントのカスタムブロックを追加

--- custom.ts
+++ custom.ts
@@ -19,12 +19,25 @@ namespace agentInspectDetect{
 /**
  * Custom blocks
  */
-//% color=#D83B01 weight=400 icon="\uf04b" block="ひらがな"
-namespace hiragana_blocks{
+//% color=#D83B01 weight=400 icon="\uf04b" block="ひらがな_プレイヤー"
+namespace hiragana_player{
 
-    //% blockId=hiragana_blocks
+    //% blockId=hiragana_player
     //% block="じぶんを $to にテレポートさせる"
     export function agentDetect(pos: Position): void {
         return player.teleport(pos);
+    }
+}
+
+/**
+ * Custom blocks
+ */
+//% color=#D83B01 weight=400 icon="\uf04b" block="ひらがな＿エージェント"
+namespace hiragana_egent{
+
+    //% blockId=hiragana_egent
+    //% block="えーじぇんとに $direction へおかせる"
+    export function agentDetect(direction: SixDirection): void {
+        return egent.place(direction);
     }
 }
